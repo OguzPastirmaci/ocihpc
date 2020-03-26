9516c1f8e03dda6f4f235f4cc846a7123525f02c
Oguz Pastirmaci <oguzp@outlook.com>
2020-03-26 14:18:13 -0700

    Add init

--- exec/init.sh
+++ exec/init.sh
@@ -21,16 +21,19 @@ Commands:
 export PACKAGE=$1
 ZIP_FILE_PATH="$OCIHPC_WORKDIR/downloaded-packages/$PACKAGE/$PACKAGE.zip"
 CONFIG_FILE_PATH="OCIHPC_WORKDIR/downloaded-packages/$PACKAGE/config.json"
-URL="https://github.com/OguzPastirmaci/ocihpc/raw/master/packages/$PACKAGE/$PACKAGE.zip"
+ZIP_FILE_URL="https://github.com/OguzPastirmaci/ocihpc/raw/master/packages/$PACKAGE/$PACKAGE.zip"
+CONFIG_FILE_URL="https://raw.githubusercontent.com/OguzPastirmaci/ocihpc/master/packages/$PACKAGE/config.json"
 
-if curl --head --silent --fail $URL > /dev/null;
+if curl --head --silent --fail $ZIP_FILE_URL > /dev/null;
  then
   echo ""
   echo "Downlading package: $PACKAGE"
   echo ""
   [ ! -d "$OCIHPC_WORKDIR/downloaded-packages/$PACKAGE" ] && mkdir -p "$OCIHPC_WORKDIR/downloaded-packages/$PACKAGE"
-  [ ! -f "$ZIP_FILE_PATH" ] && curl -sL $URL/$PACKAGE.zip -o $OCIHPC_WORKDIR/downloaded-packages/$PACKAGE/$PACKAGE.zip  > /dev/null
-  [ ! -f "$CONFIG_FILE_PATH" ] && curl -sL $URL/config.json -o $OCIHPC_WORKDIR/downloaded-packages/$PACKAGE/config.json  > /dev/null
+  [ ! -f "$ZIP_FILE_PATH" ] && curl -sL $ZIP_FILE_URL -o $OCIHPC_WORKDIR/downloaded-packages/$PACKAGE/$PACKAGE.zip  > /dev/null
+  [ ! -f "$CONFIG_FILE_PATH" ] && curl -sL $CONFIG_FILE_URL -o $OCIHPC_WORKDIR/downloaded-packages/$PACKAGE/config.json  > /dev/null
+  echo "Package $PACKAGE downloaded to $OCIHPC_WORKDIR/downloaded-packages/$PACKAGE"
+  echo ""
  else
   echo ""
   echo "The package $PACKAGE does not exist."
