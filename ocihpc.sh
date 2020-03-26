d9e2f9efc3e584a8c10a155ba1ceafbf0bdc83e3
Oguz Pastirmaci <oguzp@outlook.com>
2020-03-26 14:11:14 -0700

    Add init

--- ocihpc.sh
+++ ocihpc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export OCIHPC_WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# export COMPARTMENT_ID="ocid1.compartment.oc1..aaaaaaaafxezajxlyjxkh23ux75iqttja2qvdded3fc3v5h4kth6zvhnus3q"
 
 source "$OCIHPC_WORKDIR/common/util.sh"
 
